c360b4a689455c11ed6fef7bca57eec27b3aff42
r-carstens <100591386+r-carstens@users.noreply.github.com>
2023-12-05 14:48:48 +0000

    Updated individual_SIM_multi_diseases.py

--- individual_SIM_model/individual_SIM_with_multi_diseases/individual_SIM_multi_diseases.py
+++ individual_SIM_model/individual_SIM_with_multi_diseases/individual_SIM_multi_diseases.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 ################################################## SIMULATION CONSTANTS
 
-gammas = [1/14, 1/10] # human recovery times for variants
-sigmas = [0, 0]       # immunity breakthrough rates from variants
+gammas = [1/14, 1/10] # human recovery times for diseases
+sigmas = [0, 0]       # immunity breakthrough rates from diseases
 
 connection_prob = 0.3 # probability that a node is connected to another node
 
@@ -34,17 +34,17 @@ def get_mosquito_transmission_weights():
     return (a ** 2 * b * c * m) / mu
 
 
-def get_graph_nodes(G, all_nodes, num_variants):
+def get_graph_nodes(G, all_nodes, num_diseases):
     
-    # Randomly choosing infected nodes (currently only 1 infection per variant in the system)
-    random_infected_nodes = np.random.choice(all_nodes, size=num_variants, replace=True)
+    # Randomly choosing infected nodes (currently only 1 infection per disease in the system)
+    random_infected_nodes = np.random.choice(all_nodes, size=num_diseases, replace=True)
     
     for node in all_nodes:
         
-        # Creating a structure to store each node's states with respect to the variants
-        initial_states = np.array([susceptible for i in range(num_variants)])
+        # Creating a structure to store each node's states with respect to the diseases
+        initial_states = np.array([susceptible for i in range(num_diseases)])
         
-        # Checking if the node is infected with any variant and altering the initial states to include the infection(s)
+        # Checking if the node is infected with any disease and altering the initial states to include the infection(s)
         if len(np.where(node == random_infected_nodes)[0]) > 0:
             initial_states[np.where(node == random_infected_nodes)] = infected
         
@@ -67,14 +67,14 @@ def get_graph_edges(G):
     return G
 
 
-def initialise_graph(num_nodes, num_variants):
+def initialise_graph(num_nodes, num_diseases):
 
     # Initialising the network structure and the node labels
     G = nx.Graph()
     all_nodes = np.arange(num_nodes)
     
     # Creating the graph (these are functions to allow for more graphs to be easily constructured)
-    G = get_graph_nodes(G, all_nodes, num_variants)
+    G = get_graph_nodes(G, all_nodes, num_diseases)
     G = get_graph_edges(G)
                     
     return G
@@ -82,24 +82,24 @@ def initialise_graph(num_nodes, num_variants):
 
 ################################################## RUNNING SIMULATION
 
-def get_variant_populations(G, variant):
+def get_disease_populations(G, disease):
     
-    # Determining all nodes susceptible to and infected by the current variant
-    susceptible_nodes = [node_label for node_label, node_data in G.nodes(data=True) if susceptible in node_data['states'][variant]]
-    infected_nodes = [node_label for node_label, node_data in G.nodes(data=True) if infected in node_data['states'][variant]]
+    # Determining all nodes susceptible to and infected by the current disease
+    susceptible_nodes = [node_label for node_label, node_data in G.nodes(data=True) if susceptible in node_data['states'][disease]]
+    infected_nodes = [node_label for node_label, node_data in G.nodes(data=True) if infected in node_data['states'][disease]]
 
     return susceptible_nodes, infected_nodes
     
     
-def get_variant_recovery_data(G, infected_nodes, variant):
+def get_disease_recovery_data(G, infected_nodes, disease):
     
     # Determining potential recoveries
-    recovery_rates = [gammas[variant] for infected_node in infected_nodes]
+    recovery_rates = [gammas[disease] for infected_node in infected_nodes]
     
     return recovery_rates
 
 
-def get_variant_infection_data(G, susceptible_nodes, infected_nodes, variant):
+def get_disease_infection_data(G, susceptible_nodes, infected_nodes, disease):
     
     sources, targets, infection_rates = [], [], []
     
@@ -118,28 +118,28 @@ def get_variant_infection_data(G, susceptible_nodes, infected_nodes, variant):
     return sources, targets, infection_rates
 
 
-def get_event_rates(G, num_variants):
+def get_event_rates(G, num_diseases):
     
     # Data to store event results
-    sources, targets, rates, variants = [], [], [], []
+    sources, targets, rates, diseases = [], [], [], []
     
-    # Determining event data for all variants
-    for variant in range(num_variants):
+    # Determining event data for all diseases
+    for disease in range(num_diseases):
 
-        # Subgrouping into susceptible and infected nodes for the current variant
-        susceptible_nodes, infected_nodes = get_variant_populations(G, variant)
+        # Subgrouping into susceptible and infected nodes for the current disease
+        susceptible_nodes, infected_nodes = get_disease_populations(G, disease)
         
-        # Determining all possible event data for the current variant
-        variant_recovery_rates = get_variant_recovery_data(G, infected_nodes, variant)
-        variant_infected_sources, variant_susceptible_targets, variant_infection_rates = get_variant_infection_data(G, susceptible_nodes, infected_nodes, variant)
+        # Determining all possible event data for the current disease
+        disease_recovery_rates = get_disease_recovery_data(G, infected_nodes, disease)
+        disease_infected_sources, disease_susceptible_targets, disease_infection_rates = get_disease_infection_data(G, susceptible_nodes, infected_nodes, disease)
 
         # Storing the results
-        sources += (infected_nodes + variant_infected_sources)
-        targets += (infected_nodes + variant_susceptible_targets)
-        rates += (variant_recovery_rates + variant_infection_rates)
-        variants += [variant for i in range(len(variant_recovery_rates) + len(variant_infection_rates))]
+        sources += (infected_nodes + disease_infected_sources)
+        targets += (infected_nodes + disease_susceptible_targets)
+        rates += (disease_recovery_rates + disease_infection_rates)
+        diseases += [disease for i in range(len(disease_recovery_rates) + len(disease_infection_rates))]
         
-    return sources, targets, rates, variants
+    return sources, targets, rates, diseases
     
     
 def get_chosen_state_position(event_rates, sum_rates):
@@ -160,47 +160,47 @@ def get_chosen_time(sum_rates):
     return chosen_time
 
 
-def update_network(G, event_source, event_target, event_variant):
+def update_network(G, event_source, event_target, event_disease):
             
-    # Checking if the required variant state is susceptible
-    if G.nodes()[event_target]['states'][event_variant] == susceptible:
+    # Checking if the required disease state is susceptible
+    if G.nodes()[event_target]['states'][event_disease] == susceptible:
         
         # Setting to infected
-        G.nodes()[event_target]['states'][event_variant] = infected
+        G.nodes()[event_target]['states'][event_disease] = infected
         
-    # Checking if the required variant state is infected
-    elif G.nodes()[event_target]['states'][event_variant] == infected:
+    # Checking if the required disease state is infected
+    elif G.nodes()[event_target]['states'][event_disease] == infected:
         
         # Setting the state to recovered
-        G.nodes()[event_target]['states'][event_variant] = recovered
+        G.nodes()[event_target]['states'][event_disease] = recovered
                 
     return G
 
 
-def get_current_totals(G, num_variants):
+def get_current_totals(G, num_diseases):
     
     # Creating structures to store the current totals
-    current_data = np.zeros(shape=(num_variants, 3))
+    current_data = np.zeros(shape=(num_diseases, 3))
     
-    # Looping through each possible variant
-    for variant in range(num_variants):
+    # Looping through each possible disease
+    for disease in range(num_diseases):
         
         # Determining the population totals
-        num_susceptible = np.sum([G.nodes()[node]['states'][variant] == susceptible for node in G.nodes()])
-        num_infected = np.sum([G.nodes()[node]['states'][variant] == infected for node in G.nodes()])
-        num_recovered = np.sum([G.nodes()[node]['states'][variant] == recovered for node in G.nodes()])
+        num_susceptible = np.sum([G.nodes()[node]['states'][disease] == susceptible for node in G.nodes()])
+        num_infected = np.sum([G.nodes()[node]['states'][disease] == infected for node in G.nodes()])
+        num_recovered = np.sum([G.nodes()[node]['states'][disease] == recovered for node in G.nodes()])
         
         # Storing the current results
-        current_data[variant] = num_susceptible, num_infected, num_recovered
+        current_data[disease] = num_susceptible, num_infected, num_recovered
     
-    return current_data.reshape(num_variants * 3)
+    return current_data.reshape(num_diseases * 3)
                                  
                                    
-def run_gillespie(G, num_variants, t_max):
+def run_gillespie(G, num_diseases, t_max):
     
     # Creating file to store data
-    out_file = open('individual_SIM_variants_data.txt', 'w')
-    out_file.write('Time,' + ','.join([f'S_{variant},I_{variant},R_{variant}' for variant in range(num_variants)]) + '\n')
+    out_file = open('individual_SIM_diseases_data.txt', 'w')
+    out_file.write('Time,' + ','.join([f'S_{disease},I_{disease},R_{disease}' for disease in range(num_diseases)]) + '\n')
     
     # Setting initial conditions
     t = 0
@@ -208,7 +208,7 @@ def run_gillespie(G, num_variants, t_max):
     while t < t_max:
     
         # Determining the possible events and their rates
-        sources, targets, rates, variants = get_event_rates(G, num_variants)
+        sources, targets, rates, diseases = get_event_rates(G, num_diseases)
         sum_rates = np.sum(rates)
         
         # Checking for convergence
@@ -221,14 +221,14 @@ def run_gillespie(G, num_variants, t_max):
         # Determining which event took place
         event_source = sources[chosen_position]    # node causing the event (e.g. I_j causes an infection to S_k)
         event_target = targets[chosen_position]    # node event is occurring to (e.g. S_k becomes infected)
-        event_variant = variants[chosen_position]  # specific variant involved
+        event_disease = diseases[chosen_position]  # specific disease involved
         
         # Updating the system
-        G = update_network(G, event_source, event_target, event_variant)
+        G = update_network(G, event_source, event_target, event_disease)
         t += get_chosen_time(sum_rates)
 
         # Storing the results
-        population_results = get_current_totals(G, num_variants)
+        population_results = get_current_totals(G, num_diseases)
         out_file.write(str(t) + ',' + ','.join(population_results.astype(str)) + '\n')
 
     out_file.close()
@@ -237,7 +237,7 @@ def run_gillespie(G, num_variants, t_max):
 
 # Initialising the system
 N = 1000
-num_variants = 2
+num_diseases = 2
 
 # Setting the initial conditions
 I0, R0 = 1, 0
@@ -245,11 +245,11 @@ S0 = N - I0 - R0
 t_max = 100
 
 # Creating the graph and running the simulation
-G = initialise_graph(N, num_variants)
-G = run_gillespie(G, num_variants, t_max)
+G = initialise_graph(N, num_diseases)
+G = run_gillespie(G, num_diseases, t_max)
 
 # Reading the data
-data = pd.read_csv('individual_SIM_variants_data.txt')
+data = pd.read_csv('individual_SIM_diseases_data.txt')
 data.columns
 
 # Plotting the data
@@ -257,8 +257,8 @@ for i in range(1, len(data.columns)):
     plt.plot(data[data.columns[0]], data[data.columns[i]], label=data.columns[i])
     
 plt.legend()
-plt.title('Evolution of population sizes over time for an individual-based model \nwith %s variants' % num_variants)
+plt.title('Evolution of population sizes over time for an individual-based model \nwith %s diseases' % num_diseases)
 plt.xlabel('Time (days)')
 plt.ylabel('Population Size')
 plt.legend()
-plt.savefig('individual_SIM_variants_results.png')
+plt.show()
